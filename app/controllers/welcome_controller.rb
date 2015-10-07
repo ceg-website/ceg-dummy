ccc43e3ae2689fde0d67e5b31e7c1341b899e91c
amuthu1996 <amuthu1996@gmail.com>
2015-10-07 19:41:14 +0530

    articles and controller change

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
 	@articles = Article.order(created_at: :desc).limit(6)
 	@staffnews = Staffnews.order(created_at: :desc).limit(6)
+	@staffnewscount = Staffnews.where(created_at: < Date.today).count
+	@studentnewscount = Studentnews.count
 	@studentnews = Studentnews.order(created_at: :desc).limit(6)
 	@thirukural = Thirukural.order(created_at: :desc).limit(1)
 	@quotes = Quote.order(created_at: :desc).limit(1)
